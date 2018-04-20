00f033c97452f8754e614b6e3138cf4d9ed06cf0
jake <2426595849@qq.com>
2018-04-21 00:14:46 +0800

    页面重构

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ Page({
   data: {
     userInfo: {},
     isFail:false,
-    hotList: [{ id: 1, content: "鼻炎" }, { id: 2, content: "咳嗽" }, { id: 3, content: "失眠" }, { id: 4, content: "长痘" }, { id: 5, content: "腹泻" }, { id: 6, content: "口腔溃疡" }, { id: 7, content: "皮肤干燥" }, { id: 8, content: "高血压" }],
+    hotList: [{ id: 1, content: "二代妖精" }, { id: 2, content: "二代妖精" }, { id: 3, content: "二代妖精" }, { id: 4, content: "二代妖精" }, { id: 5, content: "二代妖精" }, { id: 6, content: "二代妖精" }, { id: 7, content: "二代妖精" }, { id: 8, content: "二代妖精" }],
     recommendList: []
   },
   //事件处理函数
@@ -56,7 +56,7 @@ Page({
   },
   onShareAppMessage: function () {
     return {
-      title: '疾病一点搜',
+      title: '网盘搜索',
       desc: '放松，愉快并便捷的了解到一些常见疾病地相关知识，做好日常预防，有助于我们健康，快乐的成长。',
       path: '/page/index/index'
     }
